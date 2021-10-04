b6b9fb56477e6c8a364c3ba93719640a350c8567
Mark Wilkinson <mark.wilkinson@dotmatics.com>
2021-10-04 11:38:05 +0100

    Add dos2unix into the mix

--- docker/api/build.sh
+++ docker/api/build.sh
@@ -3,4 +3,6 @@ cd ../..
 ./package_api.sh
 ./cleanupimages.sh
 cd docker/api
+dos2unix app.properties
+dos2unix runapp.sh
 docker build -t catsapi:latest .
--- docker/testexecutor/build.sh
+++ docker/testexecutor/build.sh
@@ -3,4 +3,6 @@ cd ../..
 ./package_listeners.sh
 ./cleanupimages.sh
 cd docker/testexecutor
+dos2unix app.properties
+dos2unix runapp.sh
 docker build -t catsexecutor:latest .
