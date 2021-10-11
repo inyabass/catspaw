8415535e6bcfb4a92b840bca15471cc7c65a7e4c
Mark <inyabass@gmail.com>
2021-10-11 19:46:57 +0100

    Remove saucetunnel

--- docker/saucetunnel/runapp.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-cd /app
-./sc.exe --tunnel-name=dotmatics --user=Mark.Wilkinson99.dotmatics --api-key=f3f0f594-a287-4919-a919-8bb105b891c7 --region=eu-central --shared-tunnel
--- docker/saucetunnel/runimage.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -it -d --rm --name saucetunnel saucetunnel:latest
